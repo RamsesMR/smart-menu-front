932fafa36340841007266d1052e5c1e304979a0c
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-27 11:11:29 +0100

    Salto rutas para ver cambios de Bootstrap a Angular
    Salt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,30 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './guards/role.guard'; // si tu guard ya valida "logueado"
+import { authGuard } from './guards/role.guard';
 import { Pagina404 } from './pages/pagina404/pagina404';
 
 export const routes: Routes = [
-
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login').then(m => m.Login),
+    loadComponent: () => import('./pages/login/login').then((m) => m.Login),
   },
 
- 
   {
     path: 'inicio',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/inicio/inicio').then(m => m.Inicio),
+    //canActivate: [authGuard],
+    loadComponent: () => import('./pages/inicio/inicio').then((m) => m.Inicio),
   },
   {
     path: 'menu',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/menu/menu').then(m => m.Menu),
+    //canActivate: [authGuard],
+    loadComponent: () => import('./pages/menu/menu').then((m) => m.Menu),
   },
   {
     path: 'pedir',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/pedir/pedir').then(m => m.Pedir),
+    //canActivate: [authGuard],
+    loadComponent: () => import('./pages/pedir/pedir').then((m) => m.Pedir),
   },
-  { path: 'barra', loadComponent: () => import('./pages/barra/barra').then(m => m.Barra) },
+  { path: 'barra', loadComponent: () => import('./pages/barra/barra').then((m) => m.Barra) },
 
-
- 
   { path: '', pathMatch: 'full', redirectTo: 'login' },
 
   { path: '404', component: Pagina404 },
