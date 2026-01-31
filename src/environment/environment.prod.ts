0561b7346395c2337675d9ce816c5dbfc8922ba9
ramsesmr <adrianramsesmunozrodriguez@gmail.com>
2026-01-31 19:01:13 +0100

    asd

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,14 +1,14 @@
 //para local
 
-// export const environment = {
-//   production: true,
-//   apiUrl: 'http://localhost:9002',
-// };
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:9002',
+};
 
 
 //produccion
 
-export const environment = {
-  production: true,
-  apiUrl: 'http://lakritas.com:8080/app3_back',
-};
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://lakritas.com:8080/app3_back',
+// };
